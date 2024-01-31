b8bdf2302ab69d4550e4b7e67904adb08339c669
Brakcc <clement.strappazzon.marques@outlook.fr>
2024-02-01 00:37:57 +0100

    rcftgvhybjknl,;m

---
+++ Assets/Scripts/UIScripts/MenuScripts/FirstSelect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIScripts.MenuScripts
+{
+    public class FirstSelect : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            GetComponent<Button>().Select();
+        }
+    }
+}
---
+++ Assets/Scripts/UIScripts/MenuScripts/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UIScripts.MenuScripts
+{
+    public class MenuManager : MonoBehaviour
+    {
+        #region fields
+
+        [SerializeField] private string gameScene;
+
+        #endregion
+
+        #region methodes
+
+        public void OnLoadScene(string arg) => SceneManager.LoadScene(arg);
+
+        public void OnQuitGame() => Application.Quit();
+
+        #endregion
+    }
+}
